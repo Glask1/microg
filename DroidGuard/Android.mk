822babfc90be4bd72dfa802578234b9e0dba7515
Glask1 <Glask1@github.com>
2019-01-09 16:40:02 -0800

    need this version of droidguard

--- DroidGuard/Android.mk
+++ DroidGuard/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := DroidGuard
-LOCAL_SRC_FILES := org.microg.gms.droidguard-14.apk
+LOCAL_SRC_FILES := org.microg.gms.droidguard-4.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
